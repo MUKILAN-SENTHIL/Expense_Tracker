47d5dabb631c108e78d715c475ef2c792da00079
MUKILAN-SENTHIL <139947051+MUKILAN-SENTHIL@users.noreply.github.com>
2026-05-16 13:58:22 +0530

    Fix import case for axiosInstance in userContext

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
-import axiosInstance from "../utils/axiosInstance";
+import axiosInstance from "../utils/axiosinstance";
 
 export const UserContext = createContext();
 
